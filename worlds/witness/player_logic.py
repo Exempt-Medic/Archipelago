84dbad80caf0fbf91db7533ff36c46e7f816b046
Exempt-Medic <ExemptMedic@Gmail.com>
2024-01-04 13:56:01 -0500

    Negating negatives is hard

--- worlds/witness/player_logic.py
+++ worlds/witness/player_logic.py
@@ -580,7 +580,7 @@ class WitnessPlayerLogic:
             "0x275FA": eps_shuffled,  # Boathouse Hook Control
             "0x17D02": eps_shuffled,  # Windmill Turn Control
             "0x17CC4": (come_to_you and not sphere_1_quarry_via_boat_snipe) or eps_shuffled,  # Quarry Elevator Panel
-            "0x17E2B": (come_to_you and not boat_shuffled) or (eps_shuffled and not sphere_1_swamp_purple_eps_snipe),  # Swamp Long Bridge
+            "0x17E2B": (come_to_you and boat_shuffled) or (eps_shuffled and not sphere_1_swamp_purple_eps_snipe),  # Swamp Long Bridge
             "0x0CF2A": False,  # Jungle Monastery Garden Shortcut
             "0x17CAA": remote_doors,  # Jungle Monastery Garden Shortcut Panel
             "0x0364E": False,  # Monastery Laser Shortcut Door
