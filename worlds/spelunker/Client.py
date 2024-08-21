ce55d97701e4ff1b460732b980dea10346c7ca78
Exempt-Medic <ExemptMedic@Gmail.com>
2024-08-21 17:26:07 -0400

    More fixups

--- worlds/spelunker/Client.py
+++ worlds/spelunker/Client.py
@@ -71,17 +71,6 @@ class SpelunkerClient(BizHawkClient):
 
     async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
 
-        if ctx.server_version.build > 0:
-            ctx.connected = True
-        else:
-            ctx.connected = False
-            ctx.refresh_connect = True
-
-        if ctx.slot_data is not None:
-            ctx.data_present = True
-        else:
-            ctx.data_present = False
-
         if ctx.server is None or ctx.server.socket.closed or ctx.slot_data is None:
             return
 
