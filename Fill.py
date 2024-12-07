60b1668576107b0d5fd4d5bf64214329594b7818
Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>
2024-12-06 20:01:59 -0500

    Update Fill.py

--- Fill.py
+++ Fill.py
@@ -454,9 +454,10 @@ def distribute_items_restrictive(multiworld: MultiWorld,
     base_state = multiworld.state.copy()
     base_state.sweep_for_advancements()
     players_with_early_locs = {loc.player for loc in fill_locations if loc.can_reach(base_state)}
-    real_players = {player for player in multiworld.player_ids if multiworld.game[player] not in ("Archipelago", "Sudoku", "Final Fantasy", "Test Game")}
+    real_players = {player for player in multiworld.player_ids if multiworld.game[player] not in ("Archipelago", "Sudoku", "Final Fantasy")}
     if len(players_with_early_locs) != len(real_players):
         print(real_players)
+        print(players_with_early_locs)
         print("CHECK HERE")
         print([multiworld.game[player] for player in real_players])
         players_without_early_locs = [multiworld.get_player_name(player) for player in real_players - players_with_early_locs]
